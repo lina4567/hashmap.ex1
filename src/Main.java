2fa4f608b4a2e796b66c27190290b4195eba1826
nina <nlina7731@gmail.com>
2023-01-22 07:41:45 -0800

    hash map exercise

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+        public class Main {
+            public static void main(String[] args) {
+                HashMap<String, Integer> examScores = new HashMap<String,Integer>();
+                examScores.put("java",80);
+                examScores.put("oprating system",78);
+                examScores.put("statics and probablity",75);
+                examScores.put("computer arch",85);
+                System.out.println(examScores.toString());
+
+            }
+
+        }
+
+
+
